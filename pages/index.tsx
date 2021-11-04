1c67351f367e69286d34b11b9a1d0d35319be7a2
Mohsen <moh.1380.1393@gmail.com>
2021-11-04 14:07:46 +0330

    new beginning

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import styles from"../styles/Home.module.scss";
+import Head from "next/head";
+import React from "react";
+
+export default function Home({ articles }: any) {
+  return (
+    <div>
+      <Head>
+        <title>Mohsen_Bg</title>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <h1>users Data</h1>
+ 
+    </div>
+  );
+}
+
